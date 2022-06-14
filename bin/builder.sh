f870d3b87c12fad04e481bd30446774d16cb7972
Michael Kriese <michael.kriese@visualon.de>
2022-06-14 12:09:45 +0200

    build: try using older build definition

--- bin/builder.sh
+++ bin/builder.sh
@@ -15,6 +15,40 @@ if [[ "${DEBUG}" == "true" ]]; then
   BUILD_ARGS="-v"
 fi
 
+SEMVER_REGEX="^(0|[1-9][0-9]*)(\.(0|[1-9][0-9]*))?(\.(0|[1-9][0-9]*))?([a-z-].*)?$"
+
+function check_semver () {
+  if [[ ! "${1}" =~ ${SEMVER_REGEX} ]]; then
+    echo "Not a semver like version - aborting: ${1}" >&2
+    exit 1
+  fi
+  export MAJOR=${BASH_REMATCH[1]}
+  export MINOR=${BASH_REMATCH[3]}
+  export PATCH=${BASH_REMATCH[5]}
+}
+
+if [[ ! -f "/usr/local/share/python-build/${VERSION}" ]]; then
+  echo "Missing build definition for ${VERSION}. Trying to find older patch build definition"
+  check_semver "${VERSION}"
+  if [[ ! "${MAJOR}" || ! "${MINOR}" || ! "${PATCH}" ]]; then
+    echo "Invalid version: ${TOOL_VERSION}" >&2
+    exit 1
+  fi
+  oldVersion=$(find /usr/local/share/python-build -type f -name "${MAJOR}.${MINOR}.*" -printf '%f\n' | grep -P '^\d+\.\d+.\d+$' | sort --version-sort -r | head -n 1)
+
+  if [[ -z $oldVersion ]]; then
+    echo "No usable definition found" >&2
+    exit 1
+  fi
+
+  echo "Using version $oldVersion"
+
+  cp "/usr/local/share/python-build/$oldVersion" "/usr/local/share/python-build/${VERSION}"
+  sed -i "s/Python-${oldVersion}/Python-${VERSION}/g" "/usr/local/share/python-build/${VERSION}"
+  sed -i "s#Python/${oldVersion}#Python/${VERSION}#g" "/usr/local/share/python-build/${VERSION}"
+  sed -i -E 's/#[a-z0-9]+"/"/' "/usr/local/share/python-build/${VERSION}"
+fi
+
 echo "Building ${NAME} ${VERSION} for ${CODENAME}-${ARCH}"
 python-build ${BUILD_ARGS} "${VERSION}" "/usr/local/${NAME}/${VERSION}"
 
